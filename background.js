d91eda786ee4719b45069d85ad61779ce9e684b8
catmmao <catmmao@gmail.com>
2019-03-10 11:04:57 +0800

    chrome翻译插件完成基本功能

---
+++ background.js
@@ -0,0 +1,22 @@
+//该文件相当于插件的后台，时刻在运行
+//该页面做到两个功能： 1.当用户在页面上选择了文字并鼠标右键后，会有该插件的一个菜单选项；2.点击该菜单选项，跳转到一个新标签页，内容是百度翻译刚才选中的文本
+
+chrome.runtime.onInstalled.addListener(function() {
+//创建一个chrome的 menu（在页面上右键时出现的菜单）
+    chrome.contextMenus.create({
+        "id": "translateMenu",
+        //title 是当用户右键页面后，在页面上显示的该插件的名字
+        "title": "翻译 %s",
+        //只有当选中了内容，右键后才会出现插件menu
+        "contexts": ["selection"]
+    });
+});
+
+//菜单被点击时触发该事件
+chrome.contextMenus.onClicked.addListener(function(info) {
+    //如果 id === ↑创建的菜单的id
+    if (info.menuItemId === 'translateMenu') {
+        //创建一个 标签页 url是：百度的翻译API 翻译内容是用户选中的内容
+        chrome.tabs.create({url: `https://fanyi.baidu.com/#lang-auto/lang-auto/${info.selectionText}`})
+    }
+})
---
+++ options.js
@@ -0,0 +1,37 @@
+//该页面是【扩展程序选项】的js代码
+
+//获取from选择框
+let fromSelect = document.querySelector('#from')
+//获取to选择框
+let toSelect = document.querySelector('#to')
+
+//从chrome storage获取存储进去的 sl(source language) from选项的值 tl(target language)to选项的值
+chrome.storage.sync.get(['sl', 'tl'], function(result) {
+    console.log(result)
+
+    //如果有存储过 source language
+    if (result.sl) {
+        //把值赋值给页面上的from选项 如：用户上次选择的from是英文，刷新页面后选项还是英文
+        fromSelect.value = result.sl.value
+    }
+
+    //如果有存储过 target language
+    if (result.tl) {
+        //把值赋值给页面上的to选项
+        toSelect.value = result.tl.value
+    }
+
+    //from选项的值改变的时候
+    fromSelect.onchange = function() {
+        //获取到当前被选择的选项的 data-type 属性
+        let key = this.selectedOptions[0].getAttribute('data-key')
+        //chrome storage 存储信息，srouce language，传入一个对象
+        chrome.storage.sync.set({'sl': {key: key, value: this.value}})
+    }
+
+    //to选项的值改变的时候
+    toSelect.onchange = function() {
+        let key = this.selectedOptions[0].getAttribute('data-key')
+        chrome.storage.sync.set({'tl': {key: key, value: this.value}})
+    }
+})
